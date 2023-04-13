ba47ac6b03d70b71f8a1bb2e7232f465ffd87d20
Davinchi-x7 <dufgvjdu@gmail.com>
2023-04-13 10:20:15 +0300

    simple list-view

---
+++ app/src/main/java/com/example/simple_listview/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.simple_listview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+    }
+}
